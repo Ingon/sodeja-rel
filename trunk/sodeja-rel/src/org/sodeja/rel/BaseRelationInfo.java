6e10d1dac06eedc07fcc3f95e4355cde1c7d22fe
sungon <sungon@a581d1d0-b9dd-11de-81bc-59952d0011f9>
2009-10-28 15:47:47 +0000

    Transaction log is now ordered

--- trunk/sodeja-rel/src/org/sodeja/rel/BaseRelationInfo.java
+++ trunk/sodeja-rel/src/org/sodeja/rel/BaseRelationInfo.java
@@ -1,7 +1,8 @@
 package org.sodeja.rel;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -9,6 +10,53 @@ import java.util.TreeSet;
 import org.sodeja.collections.PersistentSet;
 
 class BaseRelationInfo {
+	private static enum TransactionAction {
+		ADD,
+		REMOVE;
+	}
+	
+	protected static class TransactionLogItem {
+		protected final TransactionAction action;
+		protected final Entity e;
+		
+		public TransactionLogItem(TransactionAction action, Entity e) {
+			this.action = action;
+			this.e = e;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((action == null) ? 0 : action.hashCode());
+			result = prime * result + ((e == null) ? 0 : e.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			TransactionLogItem other = (TransactionLogItem) obj;
+			if (action == null) {
+				if (other.action != null)
+					return false;
+			} else if (!action.equals(other.action))
+				return false;
+			if (e == null) {
+				if (other.e != null)
+					return false;
+			} else if (!e.equals(other.e))
+				return false;
+			return true;
+		}
+	}
+	
 	public final PersistentSet<Entity> entities;
 	
 	public final BaseRelationIndex pkIndex;
@@ -16,8 +64,9 @@ class BaseRelationInfo {
 	
 	public final PersistentSet<BaseRelationListener> listeners;
 	
-	public final Set<Entity> newSet;
-	public final Set<Entity> deleteSet;
+//	public final Set<Entity> newSet;
+//	public final Set<Entity> deleteSet;
+	public final List<TransactionLogItem> txLog;
 	
 	public final Map<ForeignKey, Set<Entity>> starvingEntities; 
 	
@@ -25,14 +74,16 @@ class BaseRelationInfo {
 		this(new PersistentSet<Entity>(),
 				new BaseRelationIndex(new TreeSet<Attribute>()), new BaseRelationIndexes(),
 				new PersistentSet<BaseRelationListener>(),
-				new HashSet<Entity>(), new HashSet<Entity>(), 
+//				new HashSet<Entity>(), new HashSet<Entity>(),
+				new LinkedList<TransactionLogItem>(),
 				new HashMap<ForeignKey, Set<Entity>>());
 	}
 
 	public BaseRelationInfo(PersistentSet<Entity> entities, 
 			BaseRelationIndex pkIndex, BaseRelationIndexes fkIndexes,
 			PersistentSet<BaseRelationListener> listeners,
-			Set<Entity> newSet, Set<Entity> deleteSet, 
+//			Set<Entity> newSet, Set<Entity> deleteSet, 
+			List<TransactionLogItem> txLog,
 			Map<ForeignKey, Set<Entity>> starvedEntities) {
 
 		this.entities = entities;
@@ -42,29 +93,34 @@ class BaseRelationInfo {
 		
 		this.listeners = listeners;
 		
-		this.newSet = newSet;
-		this.deleteSet = deleteSet;
+//		this.newSet = newSet;
+//		this.deleteSet = deleteSet;
+		this.txLog = txLog;
 		
 		this.starvingEntities = starvedEntities;
 	}
 	
 	public boolean hasChanges() {
-		return ! (newSet.isEmpty() && deleteSet.isEmpty());
+		return ! txLog.isEmpty();
+//		return ! (newSet.isEmpty() && deleteSet.isEmpty());
 	}
 	
 	protected BaseRelationInfo copyDelta(PersistentSet<Entity> entities, BaseRelationIndex pkIndex, BaseRelationIndexes fkIndexes) {
 		return new BaseRelationInfo(entities, pkIndex, fkIndexes, this.listeners, 
-				this.newSet, this.deleteSet, this.starvingEntities);
+//				this.newSet, this.deleteSet, this.starvingEntities);
+				this.txLog, this.starvingEntities);
 	}
 
 	public BaseRelationInfo clearCopy() {
 		return new BaseRelationInfo(entities, pkIndex, fkIndexes, this.listeners, 
-				new HashSet<Entity>(), new HashSet<Entity>(), new HashMap<ForeignKey, Set<Entity>>());
+//				new HashSet<Entity>(), new HashSet<Entity>(), new HashMap<ForeignKey, Set<Entity>>());
+				new LinkedList<TransactionLogItem>(), new HashMap<ForeignKey, Set<Entity>>());
 	}
 
 	public BaseRelationInfo addListener(BaseRelationListener l) {
 		return new BaseRelationInfo(this.entities, this.pkIndex, this.fkIndexes, this.listeners.addValue(l), 
-				this.newSet, this.deleteSet, this.starvingEntities);
+//				this.newSet, this.deleteSet, this.starvingEntities);
+				this.txLog, this.starvingEntities);
 	}
 	
 	public BaseRelationInfo addEntity(Entity e) {
@@ -73,7 +129,8 @@ class BaseRelationInfo {
 		BaseRelationIndex newPkIndex = pkIndex.insert(e);
 		BaseRelationIndexes newFkIndexes = fkIndexes.insert(e);
 		
-		newSet.add(e);
+//		newSet.add(e);
+		txLog.add(new TransactionLogItem(TransactionAction.ADD, e));
 		
 		return copyDelta(newEntities, newPkIndex, newFkIndexes);
 	}
@@ -84,7 +141,8 @@ class BaseRelationInfo {
 		BaseRelationIndex newPkIndex = pkIndex.delete(e);
 		BaseRelationIndexes newFkIndexes = fkIndexes.delete(e);
 		
-		deleteSet.add(e);
+//		deleteSet.add(e);
+		txLog.add(new TransactionLogItem(TransactionAction.REMOVE, e));
 		
 		return copyDelta(newEntities, newPkIndex, newFkIndexes);
 	}
@@ -94,29 +152,31 @@ class BaseRelationInfo {
 		BaseRelationIndex newPkIndex = pkIndex;
 		BaseRelationIndexes newFkIndexes = fkIndexes;
 		
-		for(Entity e : versionInfo.newSet) {
-			newEntities = newEntities.addValue(e);
-			
-			newPkIndex = newPkIndex.insert(e);
-			newFkIndexes = newFkIndexes.insert(e);
-			
-			for(BaseRelationListener l : listeners) {
-				l.inserted(relation, e);
-			}
-		}
-		
-		for(Entity oe : versionInfo.deleteSet) {
-			newEntities = newEntities.removeValue(oe);
-			
-			newPkIndex = newPkIndex.delete(oe);
-			newFkIndexes = newFkIndexes.delete(oe);
-			
-			for(BaseRelationListener l : listeners) {
-				l.deleted(relation, oe);
+		for(TransactionLogItem item : versionInfo.txLog) {
+			Entity e = item.e;
+			if(item.action == TransactionAction.ADD) {
+				newEntities = newEntities.addValue(e);
+				
+				newPkIndex = newPkIndex.insert(e);
+				newFkIndexes = newFkIndexes.insert(e);
+				
+				for(BaseRelationListener l : listeners) {
+					l.inserted(relation, e);
+				}
+			} else {
+				newEntities = newEntities.removeValue(e);
+				
+				newPkIndex = newPkIndex.delete(e);
+				newFkIndexes = newFkIndexes.delete(e);
+				
+				for(BaseRelationListener l : listeners) {
+					l.deleted(relation, e);
+				}
 			}
 		}
 		
 		return new BaseRelationInfo(newEntities, newPkIndex, newFkIndexes, this.listeners, 
-				this.newSet, this.deleteSet, this.starvingEntities);
+//				this.newSet, this.deleteSet, this.starvingEntities);
+				this.txLog, this.starvingEntities);
 	}
 }
